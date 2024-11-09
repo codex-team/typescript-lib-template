0d4c78e33e1016acaf076183d58a5b8d1095be41
Peter Savchenko <specc.dev@gmail.com>
2024-11-09 16:59:41 +0300

    monorepo build setup

---
+++ packages/utils/tests/index.test.js
@@ -0,0 +1,6 @@
+import main from '../../core/src';
+describe('TypeScript library template', () => {
+    test('Should return correct result', () => {
+        expect(main()).toBe('It works!');
+    });
+});
